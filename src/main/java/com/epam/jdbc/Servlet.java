56eb9b4692cdb26a297e09c34194d6301d10ffc2
Artemiy Yamshchikov <Artemiy_Yamshchikov@epam.com>
2019-07-17 14:40:36 +0300

    servlet fix

--- src/main/java/com/epam/jdbc/Servlet.java
+++ src/main/java/com/epam/jdbc/Servlet.java
@@ -4,6 +4,7 @@ import com.epam.jdbc.datalayer.DAOFactory;
 import com.epam.jdbc.datalayer.DAOType;
 import com.epam.jdbc.datalayer.EmployeeDAO;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,46 +13,30 @@ import java.io.IOException;
 public class Servlet extends HttpServlet {
     
     private static final long serialVersionUID = 1L;
-
+    
     @Override
     public void init() {
-        System.out.println("Servlet " + this.getServletName() + " has started");
-
         DAOFactory factory = DAOFactory.getInstance(DAOType.ORACLE);
         EmployeeDAO employeeDAO = factory.getEmployeeDAO();
-
+        
         getServletContext().setAttribute("employeeDAO", employeeDAO);
     }
-
+    
     @Override
-    protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response)
-            throws IOException {
-        processRequest(request, response);
-    }
-
-
-    @Override
-    protected void doPost(HttpServletRequest request,
-                          HttpServletResponse response)
-            throws IOException {
-        processRequest(request, response);
-    }
-
-    private void processRequest(HttpServletRequest request,
-                                HttpServletResponse response)
-            throws IOException {
-
-        String firstName = request.getParameter("firstName");
-        String lastName = request.getParameter("lastName");
-
+    protected void service(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        
         EmployeeDAO empDAO = ((EmployeeDAO) getServletContext()
-                .getAttribute("employeeDAO"));
+                                                .getAttribute("employeeDAO"));
         empDAO.createEmployee(firstName, lastName);
-
-        request.getSession().setAttribute("createdEmployee",
-                "Employee " + firstName + " " + lastName + "was created");
-
-        response.sendRedirect(request.getContextPath());
+        
+        req.getSession().setAttribute("createdEmployee",
+            "Employee " + firstName + " " + lastName + "was created");
+        
+        resp.sendRedirect(req.getContextPath());
+        ;
     }
+    
 }
